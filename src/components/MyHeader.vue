85d85cea6638860c30055feca1d4099864be135c
KYV <KYV@d.c>
2024-05-20 14:52:10 +0300

    new name

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,7 +1,7 @@
 <template lang="">
 	<div class="header">
 		<div class="header__inner">
-			<h1 class="header__logo">Vue</h1>
+			<h1 class="header__logo">User Manager</h1>
 		</div>
 	</div>
 </template>
